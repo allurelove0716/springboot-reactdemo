9f6b0cc54962faca5848eb3cbd844c754e5322d0
qianmo <qianmo@qq.com>
2021-01-24 14:24:39 +0800

    创建mall项目

---
+++ mall/src/main/java/com/qianmo/mall/mbg/Generator.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C), 2015-2021, XXX有限公司
+ * FileName: Generator
+ * Author:   qianmo
+ * Date:     2021/1/24 14:14
+ * Description: 用于生产MBG的代码
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.qianmo.mall.mbg;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈用于生产MBG的代码〉
+ *
+ * @author qianmo
+ * @create 2021/1/24
+ * @since 1.0.0
+ */
+public class Generator {
+}
---
+++ mall/src/main/java/com/qianmo/mall/mbg/commentGenerator.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C), 2015-2021, XXX有限公司
+ * FileName: commentGenerator
+ * Author:   qianmo
+ * Date:     2021/1/24 14:15
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.qianmo.mall.mbg;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author qianmo
+ * @create 2021/1/24
+ * @since 1.0.0
+ */
+public class commentGenerator {
+}
